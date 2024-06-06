6ccbcf589c1c628a256100355b94f312aade0173
NGangaMahesh <nijikalgangamahesh@gmail.com>
2024-06-06 22:15:15 +0530

    add image url

--- scraper.js
+++ scraper.js
@@ -19,10 +19,11 @@ async function scrapeMedium(topic) {
           let author = item.querySelector('p')?.innerText || 'No author';
           let authorProfile = item.querySelector('article > div > div > div > div > div:nth-child(1) > div > div:nth-child(1) > div > div.fy.l > div > div > a > div > img')?.getAttribute('src')
           let publicationDate = item.querySelector('article > div > div > div > div > div:nth-child(1) > div > div:nth-child(2) > div.l.ck.ko > div.h.k.i > a > span > div')?.innerText || 'No date';
+          let imageUrl = item.querySelector('article > div > div > div > div > div:nth-child(1) > div > div:nth-child(2) > div:nth-child(2) > div > div > img')?.getAttribute('src') || '';
           let url = item.querySelector('article > div > div > div > div > div:nth-child(1)')?.getAttribute('data-href');
           let likeCount = item.querySelector('article > div > div > div > div > div:nth-child(1) > div > div.kj.j.d > div > div.am.nr.ns.nt.nu.nv.nw.nx.ny.nz.oa.ab.q > div.ab.q.cc.ob > div.pw-multi-vote-count.l.oo.op.oq.or.os.ot.ou > div > div > p > button')?.innerText || '0';
           let commentCount = item.querySelector('article > div > div > div > div > div:nth-child(1) > div > div.kj.j.d > div > div.am.nr.ns.nt.nu.nv.nw.nx.ny.nz.oa.ab.q > div.ow.l > div > div > a > p > span')?.innerText || '0';
-          results.push({ title, author, authorProfile, likeCount, commentCount, publicationDate, url });
+          results.push({ title, author, authorProfile, likeCount, commentCount, publicationDate, url, imageUrl });
         }
       });
       return results;
